1f646c669bed501d6f4602d34f79d6c5ce071347
Victor Widell <victor.widell@divid.se>
2023-09-25 15:32:35 +0200

    Fixed build.

--- src/main.ts
+++ src/main.ts
@@ -1,49 +1,46 @@
 import { makeMidiMessageFromMidiArray } from "./midi-message";
-import generatorProcessorUrl from "./custom-synth-processor.ts?url";
+import generatorProcessorUrl from "./custom-synth-processor?worker&url";
 import { customSynthProcessorKey } from "./processor-keys";
 
 function makeOscilator(context: AudioContext) {
-	const options: AudioWorkletNodeOptions = {
-		processorOptions: { sampleRate: context.sampleRate },
-	};
-	const oscillatorProcessor = new AudioWorkletNode(
-		context,
-		customSynthProcessorKey,
-		options
-	);
-	return oscillatorProcessor;
+  const options: AudioWorkletNodeOptions = {
+    processorOptions: { sampleRate: context.sampleRate },
+  };
+  const oscillatorProcessor = new AudioWorkletNode(
+    context,
+    customSynthProcessorKey,
+    options
+  );
+  return oscillatorProcessor;
 }
 
 async function main() {
-	const context = new AudioContext();
-	await context.audioWorklet.addModule(generatorProcessorUrl);
+  const context = new AudioContext();
+  await context.audioWorklet.addModule(generatorProcessorUrl);
 
-	const oscilators = new Map<number, AudioWorkletNode>();
-	const midiAccess = await navigator.requestMIDIAccess();
-	midiAccess.inputs.forEach((entry) => {
-		entry.onmidimessage = (event) => {
-			const message = makeMidiMessageFromMidiArray(
-				event.data,
-				event.timeStamp
-			);
+  const oscilators = new Map<number, AudioWorkletNode>();
+  const midiAccess = await navigator.requestMIDIAccess();
+  midiAccess.inputs.forEach((entry) => {
+    entry.onmidimessage = (event) => {
+      const message = makeMidiMessageFromMidiArray(event.data, event.timeStamp);
 
-			// Create one oscilator per note and cache them.
-			const oscillatorProcessor =
-				oscilators.get(message.number) ??
-				(() => {
-					const oscillatorProcessor = makeOscilator(context);
-					oscillatorProcessor.connect(context.destination);
-					oscilators.set(message.number, oscillatorProcessor);
-					return oscillatorProcessor;
-				})();
+      // Create one oscilator per note and cache them.
+      const oscillatorProcessor =
+        oscilators.get(message.number) ??
+        (() => {
+          const oscillatorProcessor = makeOscilator(context);
+          oscillatorProcessor.connect(context.destination);
+          oscilators.set(message.number, oscillatorProcessor);
+          return oscillatorProcessor;
+        })();
 
-			// Handle the MIDI message inside the oscilator.
-			oscillatorProcessor.port.postMessage(message);
+      // Handle the MIDI message inside the oscilator.
+      oscillatorProcessor.port.postMessage(message);
 
-			// The context refuzes to play unless `resume` is called in a user input event handler.
-			context.resume();
-		};
-	});
+      // The context refuzes to play unless `resume` is called in a user input event handler.
+      context.resume();
+    };
+  });
 }
 
 main();
